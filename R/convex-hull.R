93534386c27005e299c97c1fb2cfb0e699721120
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-09-03 10:10:00 +0200

    convex data

--- R/convex-hull.R
+++ R/convex-hull.R
@@ -7,14 +7,19 @@ add_convex_hull <- function(data, col = "black") {
   lines(x[hpts], y[hpts], col = col, lwd = 1.5)
 }
 
-#' Title
+#' Plot target with convex hull.
 #'
 #' @param data archery data.
 #' @param vars vars to split by.
 #'
 #' @examples
 #' 
+#' data(archeryData)
+#' plot_convex_hull_by(archeryData, "Date")
+#' 
 plot_convex_hull_by <- function(data, vars) {
+
+  layout(cbind(1,2), widths = c(2,1))
 
   target <- get_target_type(data)
   plot_target(target)
@@ -27,4 +32,15 @@ plot_convex_hull_by <- function(data, vars) {
     add_convex_hull(dtList[[i]], col = colors[i])
   })
   
+  desc <- apply(splData[, -ncol(splData)], 1, function(x)
+    paste(unlist(x), collapse = " - "))
+  
+  par(xpd = TRUE)
+  plot(0,0, type = "n", axes = FALSE)
+  legend(
+    "topleft",
+    desc,
+    col = colors,
+    border = NA,pch = 19
+  )    
 }
