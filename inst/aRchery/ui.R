5c7c0a56a7e4d85f716c27c2f0abe1a810de6b05
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-10-09 08:50:11 +0200

    ui

--- inst/aRchery/ui.R
+++ inst/aRchery/ui.R
@@ -2,17 +2,23 @@ library(shiny)
 library(shinydashboard)
 
 MENU <- sidebarMenu(
+    menuItem("Intro", tabName = "Intro"),
     menuItem("Grouping Mean", tabName = "GroupingMean")
 )
 
 BODY <- tabItems(
+    tabItem(tabName = "Intro",
+        shinydashboard::box(width = 6,
+            fileInput("FilePath", label = "Upload file", multiple = TRUE)
+        )
+    ),
     tabItem(tabName = "GroupingMean",
         shinydashboard::box(title = "Menu", width = 4,
             selectInput("Date", label = "Select date:", choices = NULL, selected = NULL),
             selectInput("Group size", inputId = "GroupSize", choices = NULL, selected = NULL),
             sliderInput("Slider", label = "Slider", min = 0, max = 12, value = 1)
         ),
-        shinydashboard::box(title = "Menu", width = 8,
+        shinydashboard::box(title = "Menu", width = 8, height = "75vh",
             plotOutput("Plot", width = "100%", height = "70vh")
         )
     )
