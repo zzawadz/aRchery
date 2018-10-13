b0c0e131dafaff2da0b13fd4ecf4aa7d65c4d3a0
Zygmunt Zawadzki <zygmunt@zstat.pl>
2018-10-13 12:42:24 +0200

    fix file uploading

--- R/read-data.R
+++ R/read-data.R
@@ -8,21 +8,38 @@
 #' 
 read_archery_files <- function(files) {
   
-  allFiles <- suppressWarnings(suppressMessages(lapply(files, function(x) 
-    {
-      firstLine <- readLines(x, n = 1)
-      sep <- if(length(strsplit(firstLine, split = ",")[[1]]) > 2) "," else ";"
-      
-      res <- readr::read_delim(x, delim = sep)
-      date <- res[["Date"]]
-      
-      date <- lubridate::ymd(date)
-      if(all(is.na(date))) {
-        date <- lubridate::dmy(res[["Date"]])
-      }
-      res[["Date"]] <- date
-      res
+  
+  read_csv_file <- function(x) 
+  {
+    firstLine <- readLines(x, n = 1)
+    sep <- if(length(strsplit(firstLine, split = ",")[[1]]) > 2) "," else ";"
+    
+    res <- readr::read_delim(x, delim = sep)
+    date <- res[["Date"]]
+    
+    date <- lubridate::ymd(date)
+    if(all(is.na(date))) {
+      date <- lubridate::dmy(res[["Date"]])
     }
+    res[["Date"]] <- date
+    res
+  }
+  
+  read_excel_file <- function(x) {
+    dt <- readxl::read_excel(x)
+    dt[["Date"]] <- as.Date(dt[["Date"]])
+    dt
+  }
+  
+  
+  allFiles <- suppressWarnings(suppressMessages(lapply(files, function(x) {
+    result <- if(tools::file_ext(x) == "xlsx") {
+      read_excel_file(x)
+    } else {
+      read_csv_file(x)
+    }
+    result
+  }
   )))
   allData  <- dplyr::bind_rows(allFiles)
   allData  <- seplyr::arrange_se(allData, c("Date", "End"))
